87d5c81f390a18a1ec05c6a06a5741e80664250b
luizantoniocardoso <la200.o.sc@gmail.com>
2025-06-25 10:52:13 -0300

    inicializando spring

---
+++ src/main/java/com/example/Projeto_final_back_end/ProjetoFinalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_final_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projeto_final_back_end/ProjetoFinalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_final_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
